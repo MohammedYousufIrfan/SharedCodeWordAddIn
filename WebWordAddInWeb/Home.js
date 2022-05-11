9785bc51b6a124cda34817df6cf64885e04ca119
Yousuf irfan <Yousuf.irfan@rsystems.com>
2022-05-11 12:59:24 +0530

    .

--- WebWordAddInWeb/Home.js
+++ WebWordAddInWeb/Home.js
@@ -315,11 +315,6 @@ function Bullet_New() {
     Word.run(function (context) {
         let paragraphs = context.document.body.paragraphs;
         paragraphs.load("items");
-        //var blankParagraph = context.document.body.paragraphs.getLast().insertParagraph("", "After");
-        //let list = paragraphs.items[1].startNewList();
-        //context.sync();
-        //// insert the list at the start location
-        //let myFirstItem = list.insertParagraph("Mohammed Yousuf", Word.InsertLocation.start)
 
         var secondParagraph;
         return context.sync().then(function () {
@@ -330,14 +325,14 @@ function Bullet_New() {
             let list = paragraphs.items[1].startNewList();
 
             // insert the list at the start location
-             list.insertParagraph("My 0 item at start", Word.InsertLocation.start)
+             list.insertParagraph("My 0 item at start1", Word.InsertLocation.start)
         }).then(context.sync).then(function () {
             secondParagraph.font.set({
                 name: "Courier New",
                 bold: true,
                 size: 30,
             });
-             list.insertParagraph("My 0 item at start", Word.InsertLocation.start)
+             list.insertParagraph("23", Word.InsertLocation.start)
         })
         //return context.sync();
     }).catch(function (error) {
